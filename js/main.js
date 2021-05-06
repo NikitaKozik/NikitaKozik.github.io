87a3070dbdb5c92e4cd35a36a6f80d260890115d
Nikita Kozik <kozik@internet.ru>
2021-05-07 00:06:52 +0300

    Almost done. Added support for mobile devices

--- js/main.js
+++ js/main.js
@@ -1,27 +1,63 @@
-function toggleMobNav() {
+window.onload = () => {
+//dropdown
+function toggleDropdown() {
 
-    const navButton = document.getElementsByClassName('navigation-hamburger')[0],
-        nav = document.querySelector('.nav-links');
+    const toggleButton = document.querySelector('.open-dropdown'),
+          toggleContainer = document.querySelector('.dropdown');
 
-    navButton.addEventListener('click', (e) => {
-        nav.classList.toggle('mobile');
-
+    toggleButton.addEventListener('click', (e) => {
+        toggleContainer.classList.toggle('show');
+        toggleButton.classList.add('active');
         e.preventDefault();
         e.stopPropagation();
-
     });
 
     window.addEventListener('click', (e) => {
-        if (!nav.contains(e.target) && !navButton.contains(e.target)) {
-            nav.classList.remove('mobile');
+        if (!toggleContainer.contains(e.target) && !toggleButton.contains(e.target)) {
+            toggleContainer.classList.remove('show');
+            toggleButton.classList.remove('active');
             e.preventDefault();
             e.stopPropagation();
         }
     });
 
-
-
-
 }
 
-toggleMobNav();
+toggleDropdown();
+
+//toggle mobile nav
+function toggleMobileNav() {
+    const toggleMobileNavButton = document.querySelector('.toggle-navigation'),
+          navigation = document.querySelector('.links');
+
+          toggleMobileNavButton.addEventListener('click', (e) => {
+            navigation.classList.toggle('show');
+            
+            e.preventDefault();
+        });
+
+        window.addEventListener('click', (e) => {
+            if (!navigation.contains(e.target) && !toggleMobileNavButton.contains(e.target)) {
+                navigation.classList.remove('show');
+                e.preventDefault();
+            }
+        });
+}
+
+toggleMobileNav();
+
+//crop card title 
+
+function cropTitle() {
+    const cardTitle = document.querySelectorAll('.card-title');
+
+    for(let i = 0; i < cardTitle.length; i++) {
+        if(cardTitle[i].innerText.length > 60) {
+            cardTitle[i].textContent =  cardTitle[i].innerText.substring(0,60) + '...';
+        }
+    }
+}
+
+cropTitle();
+
+};
